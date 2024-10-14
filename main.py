716126a40388e2389f173ec753aa6828af49d0bf
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-10-15 00:09:45 +0300

    Update main.py
    Adding template routes

---
+++ main.py
@@ -0,0 +1,18 @@
+def home():
+  return render_template('<h1>Home Page</h1>',title='Home Page')
+
+def about():
+return render_template('<h1>About Page</h1>',title='About Page')
+
+def register():
+return render_template('<h1>Register Page</h1>',title='Register Page')
+
+def login():
+return render_template('<h1>Login Page</h1>',title='Login Page')
+
+def logout():
+return render_template('<h1>Logout Page</h1>',title='Logout Page')
+
+def profile():
+return render_template('<h1>Profile Page</h1>',title='Profile Page')
+  
