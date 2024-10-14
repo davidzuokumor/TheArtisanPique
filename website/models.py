0c173c34ec616d872176e3cd1eadeb4793d443d6
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-10-15 00:17:04 +0300

    Update models.py

---
+++ website/models.py
@@ -0,0 +1,7 @@
+class User(#db.Model):
+    # id = db.Column(db.Integer,primary_key)
+    # name = db.Column(db.String(50),nullable=False)
+    # age = db.Column(db.Integer,nullable=True)
+    # date_created = db.Column(db.DateTime,default=datetime.time(),nullable=False)
+class ArtPiece(#db.Model):
+    pass
