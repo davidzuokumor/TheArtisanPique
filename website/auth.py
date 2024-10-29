3372286adf54e04b639d70c6ee53e60598565908
Ian Ganza <144003894+i-ganza007@users.noreply.github.com>
2024-10-29 20:43:10 +0000

    Working itttt

--- website/auth.py
+++ website/auth.py
@@ -1,35 +1,50 @@
-from flask import Blueprint, render_template, url_for, redirect, request
-from .forms import SignUpForm , LoginForm
+from flask import Blueprint, render_template, url_for, redirect, request, flash
+from flask_mail import Message
+from werkzeug.security import generate_password_hash
+from .forms import SignUpForm, LoginForm
+from .models import User
+from website import db, mail  
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods = ['GET', 'POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     loginform = LoginForm()
-    data = request.form
-
-    if request.method == 'POST':
-        # leave this empty for now
-        print(data)
+    if loginform.validate_on_submit(): 
+        flash('Login successful!', 'success')
         return redirect(url_for('routes.home'))
-    return render_template("login.html",loginform=loginform)
+    return render_template("login.html", loginform=loginform)
 
 @auth.route('/logout')
 def logout():
+    flash('You have been logged out.', 'info')
     return "<p>Logout</p>"
 
-@auth.route('/sign_up', methods = ['GET', 'POST'])
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     signupform = SignUpForm()
-    if request.method == "POST":
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        confirm_password = request.form.get('confirm-password')
-        role = request.form.get('role')
-
-        #implement validation login here and the password hashing too
-
+    if signupform.validate_on_submit(): 
+        user = User(
+            username=signupform.username.data,
+            age=signupform.age.data,
+            email=signupform.email.data,
+            role=signupform.role.data,
+            password=generate_password_hash(signupform.password.data)  
+        )
+        
+        db.session.add(user)  
+        db.session.commit() 
+        
+        
+        msg = Message(
+            subject='Confirming Email Registration',
+            sender='artisan@email.com',
+            recipients=[user.email]
+        )
+        msg.body = 'Thank you for signing up for this account'
+        mail.send(msg)
+        
+        flash('You have registered successfully!', 'success')
         return redirect(url_for('auth.login'))
-    else:
-        return render_template("signup.html",signupform=signupform)
+    
+    return render_template("signup.html", signupform=signupform)
